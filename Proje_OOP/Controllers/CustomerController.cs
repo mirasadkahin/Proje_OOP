e117c144967cf390802fba1e5c525321a24a3723
Mirac Sadık Sahin <mirasadkahin77@gmail.com>
2024-10-26 15:24:27 +0300

    Customer bitti

--- Proje_OOP/Controllers/CustomerController.cs
+++ Proje_OOP/Controllers/CustomerController.cs
@@ -10,7 +10,7 @@ namespace Proje_OOP.Controllers
         public IActionResult Index()
         {
             var values = context.Customers.ToList();
-            return View();
+            return View(values);
         }
         [HttpGet]
         public IActionResult AddCustomer()
@@ -20,28 +20,38 @@ namespace Proje_OOP.Controllers
         [HttpPost]
         public IActionResult AddCustomer(Customer p)
         {
-            context.Add(p);
-            context.SaveChanges();
-            return RedirectToAction("Index");
+            if (p.CustomerName.Length >= 6 && p.CustomerCity != "" && p.CustomerCity.Length >= 3)
+            {
+                context.Add(p);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.message = "hatalı kullanım";
+                return View();
+            }
+              
         }
         public IActionResult DeleteCustomer(int id)
         {
             var value = context.Customers.Where(x => x.CustomerID == id).FirstOrDefault();
             context.Remove(value);
+            context.SaveChanges();
             return RedirectToAction("Index");
         }
         [HttpGet]
         public IActionResult UpdateCustomer(int id)
         {
-            var value = context.Customers.Where(x =>x.CustomerID == id).FirstOrDefault();
+            var value = context.Customers.Where(x => x.CustomerID == id).FirstOrDefault();
             return View(value);
         }
         [HttpPost]
         public IActionResult UpdateCustomer(Customer p)
         {
-            var value = context.Customers.Where(x => x.CustomerID ==  p.CustomerID).FirstOrDefault();
-            value.CustomerCity= p.CustomerCity;
-            value.CustomerName= p.CustomerName;
+            var value = context.Customers.Where(x => x.CustomerID == p.CustomerID).FirstOrDefault();
+            value.CustomerCity = p.CustomerCity;
+            value.CustomerName = p.CustomerName;
             context.SaveChanges();
             return RedirectToAction("Index");
         }
