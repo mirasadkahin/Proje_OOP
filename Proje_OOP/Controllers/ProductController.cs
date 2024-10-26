54fff39141050d02a65b01a3a3535de9f0228e65
Mirac Sadık Sahin <mirasadkahin77@gmail.com>
2024-10-26 14:01:05 +0300

    Product Kısmı Bitti

---
+++ Proje_OOP/Controllers/ProductController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Proje_OOP.Entity;
+using Proje_OOP.ProjeContext;
+
+namespace Proje_OOP.Controllers
+{
+    public class ProductController : Controller
+    {
+        Context context = new Context();
+        public IActionResult Index()
+        {
+            var values = context.Products.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddProduct(Product p)
+        {
+            context.Add(p);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteProduct(int id)
+        {
+            var value = context.Products.Where(x => x.ProductID == id).FirstOrDefault();
+            context.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult UpdateProduct(int id)
+        {
+            var value = context.Products.Where(x =>x.ProductID == id).FirstOrDefault();
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult UpdateProduct(Product p)
+        {
+            var value = context.Products.Where(x => x.ProductID == p.ProductID).FirstOrDefault();
+            value.ProductName= p.ProductName;
+            value.ProdıctPrice = p.ProdıctPrice;
+            value.ProductStock = p.ProductStock;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        
+    }
+}
